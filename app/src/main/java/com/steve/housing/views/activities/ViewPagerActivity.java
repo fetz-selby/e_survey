8f8714d0e2676fa6cd8d0834bbc87fb368941223
stevandoh <stevandoh@gmail.com>
2017-04-25 14:58:38 +0000

    view pager trial1

--- app/src/main/java/com/steve/housing/views/activities/ViewPagerActivity.java
+++ app/src/main/java/com/steve/housing/views/activities/ViewPagerActivity.java
@@ -3,13 +3,57 @@ package com.steve.housing.views.activities;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Toast;
 
 import com.steve.housing.R;
+import com.steve.housing.views.adapters.SmartFragmentStatePagerAdapter;
+import com.steve.housing.views.fragment.PersonalDetailsFormFragment;
 
 public class ViewPagerActivity extends AppCompatActivity {
+    private SmartFragmentStatePagerAdapter adapterViewPager;
+
+
+//    FragmentPagerAdapter adapterViewPager;
+
+    public static class MyPagerAdapter extends  SmartFragmentStatePagerAdapter {
+        private static int NUM_ITEMS = 3;
+
+        public MyPagerAdapter(FragmentManager fragmentManager) {
+            super(fragmentManager);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            switch (position) {
+                case 0: // Fragment # 0 - This will show FirstFragment
+                    return PersonalDetailsFormFragment.newInstance("Page # 1", 0);
+//                case 1: // Fragment # 0 - This will show FirstFragment different title
+//                    return FirstFragment.newInstance(1, "Page # 2");
+//                case 2: // Fragment # 1 - This will show SecondFragment
+//                    return SecondFragment.newInstance(2, "Page # 3");
+                default:
+                    return null;
+
+            }
+        }
+
+        @Override
+        public int getCount() {
+            return 0;
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return "Page " + position;
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +70,34 @@ public class ViewPagerActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+        ViewPager vpPager = (ViewPager) findViewById(R.id.vpPager);
+        adapterViewPager = new MyPagerAdapter(getSupportFragmentManager());
+        vpPager.setAdapter(adapterViewPager);
+
+        // Attach the page change listener inside the activity
+        vpPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+
+            // This method will be invoked when a new page becomes selected.
+            @Override
+            public void onPageSelected(int position) {
+                Toast.makeText(ViewPagerActivity.this,
+                        "Selected page position: " + position, Toast.LENGTH_SHORT).show();
+            }
+
+            // This method will be invoked when the current page is scrolled
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+                // Code goes here
+            }
+
+            // Called when the scroll state changes:
+            // SCROLL_STATE_IDLE, SCROLL_STATE_DRAGGING, SCROLL_STATE_SETTLING
+            @Override
+            public void onPageScrollStateChanged(int state) {
+                // Code goes here
+            }
+        });
     }
+
 
 }
---
+++ app/src/main/java/com/steve/housing/views/fragment/PersonalDetailsFormFragment.java
@@ -0,0 +1,87 @@
+package com.steve.housing.views.fragment;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.steve.housing.R;
+
+
+public class PersonalDetailsFormFragment extends Fragment {
+    // Store instance variables
+    private String title;
+    private int page;
+    private OnFragmentInteractionListener mListener;
+
+    public PersonalDetailsFormFragment() {
+        // Required empty public constructor
+    }
+
+
+    public static PersonalDetailsFormFragment newInstance(String title, int page) {
+        PersonalDetailsFormFragment fragment = new PersonalDetailsFormFragment();
+        Bundle args = new Bundle();
+        args.putInt("someInt", page);
+        args.putString("someTitle", title);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        page = getArguments().getInt("someInt", 0);
+        title = getArguments().getString("someTitle");
+
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_personal_details_form, container, false);
+        TextView tvLabel = (TextView) view.findViewById(R.id.title);
+        tvLabel.setText(page + " -- " + title);
+        return view;
+
+    }
+
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        if (context instanceof OnFragmentInteractionListener) {
+            mListener = (OnFragmentInteractionListener) context;
+        } else {
+            throw new RuntimeException(context.toString()
+                    + " must implement OnFragmentInteractionListener");
+        }
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        mListener = null;
+    }
+
+    /**
+     * This interface must be implemented by activities that contain this
+     * fragment to allow an interaction in this fragment to be communicated
+     * to the activity and potentially other fragments contained in that
+     * activity.
+     * <p>
+     * See the Android Training lesson <a href=
+     * "http://developer.android.com/training/basics/fragments/communicating.html"
+     * >Communicating with Other Fragments</a> for more information.
+     */
+    public interface OnFragmentInteractionListener {
+        // TODO: Update argument type and name
+        void onFragmentInteraction(Uri uri);
+    }
+}
