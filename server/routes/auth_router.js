128c64c941da36ff9ae9a714a79abf5140822939
Gideon Kombian <gidkom@yahoo.co.uk>
2017-05-08 11:00:07 +0000

    agent login

--- server/routes/auth_router.js
+++ server/routes/auth_router.js
@@ -1,4 +1,5 @@
 var express = require('express'),
+	Agent 	= require('../models/agent'),
 	User 	= require('../models/user');
 
 var routes = function(){
@@ -22,7 +23,7 @@ var routes = function(){
 
 
 
-			User.findOne({$or: [{phone: email},{email: email}]} )
+			User.findOne({{email: email})
 			.then(function(user){
 		        // if no user is found, return the message
 		        if (!user)
@@ -40,7 +41,38 @@ var routes = function(){
 		    });
 
 	    });
+        
+    authRouter.route('/agents/login')
+	    .post(function(req, res){
+	    	var email = req.body.email,
+				password = req.body.password;
 
+			req.checkBody('email', 'email field is required').notEmpty();
+			req.checkBody('password', 'password field is required').notEmpty();
+
+		    var errors = req.validationErrors();
+			if (errors) return res.status(422).json({success: false, errors: errors});
+
+
+
+			Agent.findOne({$or: [{phone: email},{email: email}]} )
+			.then(function(agent){
+		        // if no user is found, return the message
+		        if (!user)
+		            return res.status(422).json({success: false, message:'Invalid username/password.'});
+
+		        // if the user is found but the password is wrong
+		        if (!user.validPassword(password))
+		            return res.status(422).json({success: false, message:'Invalid username/password.'});
+
+
+		        // login successful
+		        user = user.toObject();
+		        delete user.password;
+		        return res.json({ success: true, message:'Login successful', agent: agent });
+		    });
+
+	    });
 
 	authRouter.route('/signup')
         .post(function(req, res){
